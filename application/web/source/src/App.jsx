51f40931ca6c71dfa813a1b023411704c2a4fbb5
Castillo <joncastillo@teksystems.com>
2023-03-27 15:56:36 -0500

    app(listView): created the list view page and added the route to apps

--- application/web/source/src/App.jsx
+++ application/web/source/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./componets/landing/Landing";
+import ListView from "./componets/listView/List"
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing/>} />
+          <Route path="show-list" element={<ListView/>} />
         </Routes>
       </BrowserRouter>
     </div>
